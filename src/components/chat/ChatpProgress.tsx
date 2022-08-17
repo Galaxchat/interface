87f99c2d23c932cc97cbdf5f9d14554d25864979
sunnyzhifei <sunnyzhifei@gmail.com>
2022-08-17 21:45:08 +0800

    changeProgress

--- src/components/chat/ChatpProgress.tsx
+++ src/components/chat/ChatpProgress.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import styled from "styled-components/macro";
 import "react-chat-elements/dist/main.css";
 import './style.css';
@@ -7,22 +7,40 @@ import { BigNumber } from 'ethers'
 
 
 export default function ChatProgress(props: any) {
+  const [currentFund, setCurrentFund] = useState<any>()
+  const [minFund, setMinFund] = useState<any>()
   const { contract, account, chatRoomAddress } = props
-  console.log(contract)
+
+
+  const changeProgress = useCallback(() => {
+    const percentage = (currentFund / minFund * 100).toString()
+    const element = document.querySelector(".g-progress") as HTMLElement
+    if (element && percentage){
+      // element.style.setProperty("--progress", percentage)
+      element.style.setProperty("background", `linear-gradient(90deg, #0f0, #fb7962 ${percentage}%, transparent 0)`)
+    }
+  }, [currentFund, minFund])
+
 
   useEffect(() => {
-    if (contract && account && chatRoomAddress) {
-      const currentFund = contract.chatroomStatus(chatRoomAddress)
-      console.log("currentFund:", currentFund)
+    const getProgress = async () => {
+      if (contract && account && chatRoomAddress) {
+        const chatroomStatus = await contract.chatroomStatus(chatRoomAddress)
+        // setCurrentFund(parseFloat(chatroomStatus.totalFund.toString()) / 100000000000000000)
+        setCurrentFund(0.21)
+
+        const getMinFund = await contract.minETHAmount()
+        setMinFund(parseFloat(getMinFund.toString()) / 100000000000000000)
+      }
     }
-  }, [contract, account, chatRoomAddress])
+    getProgress()
+    changeProgress()
+  }, [contract, account, chatRoomAddress, currentFund, minFund])
 
   return (
     <>
-      <div className="progress-bar">
-        <div className="progress-bg">
-        </div>
-        0.1ETH / 1 ETH
+      <div className="g-progress">
+        {currentFund} ETH / {minFund} ETH
       </div>
     </>
   )
